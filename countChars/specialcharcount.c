f83a90e09ba8cf9ec97d3a5b3af972a8bfc9aa3c
Schuyler <103162622+schuyler-w@users.noreply.github.com>
2023-08-08 16:21:27 -0700

    Update specialcharcount.c

--- countChars/specialcharcount.c
+++ countChars/specialcharcount.c
@@ -2,12 +2,6 @@
  * specialcharcount.c - this file implements the specialcharcount function.
  */
 
-/* AUTHOR: SCHUYLER WANG
- * cssc1766@edoras.sdsu.edu
- * DATE: JUNE 30, 2023
- */
-
-
 #include <stdio.h>
 #include <dirent.h>
 #include <string.h>
